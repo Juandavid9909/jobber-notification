3b82fa2e46074aacce41d0dc2c360c03b51c2231
Juan David Varela Requejo <juan.varela01+globant@globant.com>
2025-08-28 21:25:47 -0500

    Variables de entorno y archivo de configuración

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config({});
+
+class Config {
+  public CLIENT_URL: string | undefined;
+  public ELASTIC_PASSWORD: string | undefined;
+  public ELASTIC_SEARCH_URL: string | undefined;
+  public ELASTIC_USERNAME: string | undefined;
+  public NODE_ENV: string | undefined;
+  public RABBITMQ_ENDPOINT: string | undefined;
+  public SENDER_EMAIL_PASSWORD: string | undefined;
+  public SENDER_EMAIL: string | undefined;
+
+  constructor() {
+    this.CLIENT_URL = process.env.CLIENT_URL ?? '';
+    this.ELASTIC_PASSWORD = process.env.ELASTIC_PASSWORD ?? '';
+    this.ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL ?? '';
+    this.ELASTIC_USERNAME = process.env.ELASTIC_USERNAME ?? '';
+    this.NODE_ENV = process.env.NODE_ENV ?? '';
+    this.RABBITMQ_ENDPOINT = process.env.RABBITMQ_ENDPOINT ?? '';
+    this.SENDER_EMAIL = process.env.SENDER_EMAIL ?? '';
+    this.SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD ?? '';
+  }
+}
+
+export const config: Config = new Config();
